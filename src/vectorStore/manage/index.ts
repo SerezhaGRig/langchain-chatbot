e1e00b84b685f6c33232232979c961ea32d93ffd
SerezhaGRig <serojjan2000@gmail.com>
2024-07-04 12:26:10 +0400

    feat: adds function to read all data

--- src/vectorStore/manage/index.ts
+++ src/vectorStore/manage/index.ts
@@ -67,6 +67,17 @@ export class VectorStore {
     const collection = this.client.collections.get(collectionName);
     return collection.query.nearText(nearText);
   };
+  readAllFromCollection = async (
+    collectionName: string,
+    skip: string,
+    take: string,
+  ) => {
+    const collection = this.client.collections.get(collectionName);
+    return collection.query.fetchObjects({
+      limit: Number.parseInt(take, 10),
+      offset: Number.parseInt(skip, 10),
+    });
+  };
   static getClient = async (url: string, key: string) => {
     return await weaviate.connectToWeaviateCloud(url, {
       authCredentials: new weaviate.ApiKey(key),
